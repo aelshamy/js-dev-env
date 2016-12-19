5f4713db3ec4b59b225cda2dd39511dbe22ed3ae
ِAmr Elshamy <amr-hussien@hotmail.com>
2016-12-19 14:51:08 +0200

    disable browser sync

--- buildScript/srcServer.js
+++ buildScript/srcServer.js
@@ -1,20 +1,47 @@
 import express from "express";
 import path from "path";
 import open from "open";
-import browserSync from "browser-sync";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
+// import browserSync from "browser-sync";
+
+
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
 
-if (app.get('env') == 'development') {
-    
-    var bs = browserSync({
-        server: 'src',
-        tunnel: 'ajmoro',
-        logSnippet: false,
-    });
-    app.use(require('connect-browser-sync')(bs));
-}
+app.use(require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+// if (app.get('env') == 'development') {
+
+//     var bs = browserSync({
+//         server: './',
+//         tunnel: 'ghobashy',
+//         files: ['buildScript', 'src'],
+//         logSnippet: false,
+//     });
+//     app.use(require('connect-browser-sync')(bs));
+// }
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
+
+app.get('/users', function(req, res) {
+    res.json([
+        { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+        { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
+        { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com" }
+    ]);
+});
+
+app.listen(port, function(error) {
+    if (error) {
+        console.log(error);
+    } else {
+        open("http://localhost:" + port);
+    }
+})
