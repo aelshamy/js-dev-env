ce80f182410e6b601ee5b1b6091b18e1781576cb
ِAmr Elshamy <amr-hussien@hotmail.com>
2016-12-20 11:31:13 +0200

    create production code.

--- buildScript/srcServer.js
+++ buildScript/srcServer.js
@@ -26,11 +26,11 @@ app.use(require("webpack-dev-middleware")(compiler, {
 //     app.use(require('connect-browser-sync')(bs));
 // }
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.get('/users', function(req, res) {
+app.get('/users', function (req, res) {
     res.json([
         { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
         { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
@@ -38,7 +38,7 @@ app.get('/users', function(req, res) {
     ]);
 });
 
-app.listen(port, function(error) {
+app.listen(port, function (error) {
     if (error) {
         console.log(error);
     } else {
